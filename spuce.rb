e02d0840a65715c5ebc10648a253dab5ed4600b0
Tony Kirke <tony@pyspuc.com>
2015-12-11 11:27:10 -0800

    update spuce to 0.4.2

--- spuce.rb
+++ spuce.rb
@@ -2,8 +2,8 @@ class Spuce < Formula
   desc "A C++ library for DSP filters"
   homepage "http://pyspuc.com"
   head "https://github.com/audiofilter/spuce.git"
-  url "https://github.com/audiofilter/spuce/archive/0.4.1.tar.gz"
-  version "0.4.1"
+  url "https://github.com/audiofilter/spuce/archive/0.4.2.tar.gz"
+  version "0.4.2"
   sha256 "7e0cf0b5b0212be7347fbc620b61250fdf2701ad9425831c00ea4b26aee7c1e1"
   depends_on "cmake" => :build
 
